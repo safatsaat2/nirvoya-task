288c8baabb46099ad9a6802bc9688bb6193bc930
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-10-08 14:56:26 +0600

    category section mobile responsive

--- app/category/Category.jsx
+++ app/category/Category.jsx
@@ -15,19 +15,22 @@ const Category = () => {
 
 
     return (
-        <div className='mt-[50px]'>
-            <h3 className='text-[26px] text-[#383838] font-medium'>
+        <div className=' mt-[30px] mx-4 lg:mx-0 lg:mt-[50px]'>
+            <h3 className='text-[26px] text-[#383838] font-medium hidden lg:block'>
                 Category
             </h3>
-            <div className='grid grid-cols-5 justify-between gap-[13px] mt-8'>
-                {category.map(cat => 
-                <div key={cat.key} className='w-[270px] cursor-pointer h-[65px] category-box flex justify-between items-center pl-[16px] pr-[22px]'>
-                    <div className='flex items-center gap-x-[10px]'>
-                        <img src={cat?.icon} alt="" />
-                        <p>{cat?.name}</p>
+            <h3 className='text-[16px] text-[#383838] leading-[20px] font-medium block lg:hidden'>
+                Main categories
+            </h3>
+            <div className='grid grid-cols-2 lg:grid-cols-5 justify-between gap-[13px] mt-4 lg:mt-8'>
+                {category.map(cat =>
+                    <div key={cat.key} className='w-full lg:w-[270px] cursor-pointer h-[65px] category-box flex justify-between items-center pl-[16px] pr-[22px]'>
+                        <div className='flex items-center gap-x-[10px]'>
+                            <img className='w-[26px] lg:w-[30px]' src={cat?.icon} alt="" />
+                            <p className='text-sm lg:text-base'>{cat?.name}</p>
+                        </div>
+                        <img className='w-4 lg:w-5' src="https://i.ibb.co/NjnzPYV/Arrow-Right-2.png" alt="" />
                     </div>
-                    <img className='w-5' src="https://i.ibb.co/NjnzPYV/Arrow-Right-2.png" alt="" />
-                </div>
                 )}
             </div>
         </div>
